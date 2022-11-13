ace193164da2dc2da66ac90068c15e976de70122
DennisLCastillo <55577545+DennisLCastillo@users.noreply.github.com>
2022-11-13 20:04:11 +0100

    Second Master Commit

--- src/main/java/GitDemBranchTest.java
+++ src/main/java/GitDemBranchTest.java
@@ -1,6 +1,7 @@
 public class GitDemBranchTest {
     public static void main (String[] args ) {
         System.out.println("Hello World!");
+        System.out.println("Tjekker om Master Branch bevæger sig");
 
     }
 
